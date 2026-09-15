20d8470d1b38c4d0c359b186c636a24b02a6ae58
Rufaro Chirume <ruffych594@gmail.com>
2026-09-15 12:44:09 +0200

    week2v1

---
+++ scripts/temples.js
@@ -0,0 +1,24 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+document.querySelector("#currentyear").textContent = currentYear;
+
+// Display the date the document was last modified
+document.querySelector("#lastModified").textContent =
+  `Last Modified: ${document.lastModified}`;
+
+// Hamburger menu
+const menuButton = document.querySelector("#menu-button");
+const navigation = document.querySelector("nav");
+
+menuButton.addEventListener("click", () => {
+  navigation.classList.toggle("open");
+
+  if (navigation.classList.contains("open")) {
+    menuButton.textContent = "✕";
+    menuButton.setAttribute("aria-label", "Close navigation menu");
+  } else {
+    menuButton.textContent = "☰";
+    menuButton.setAttribute("aria-label", "Open navigation menu");
+  }
+});
